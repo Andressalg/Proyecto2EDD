ee237746962e8176800d75f15bf7d56298fe173e
salomegarcia0 <130935482+salomegarcia0@users.noreply.github.com>
2024-06-22 10:16:24 -0400

    ArChivos Interfaz

---
+++ src/Main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import Ventanas.Ventana1;
+
+/**
+ *
+ * @author salom
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Ventana1 v1 = new Ventana1();
+        v1.setLocationRelativeTo(null);
+        v1.setVisible(true);
+    }
+}
