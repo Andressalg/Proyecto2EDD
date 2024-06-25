5afc5742bf626d5bd16e5cc92ecc2eb430d25612
javiercillx <javier.aguilar@correo.unimet.edu.ve>
2024-06-25 19:06:22 -0400

    cambio 2.0

--- LectorArchivos/src/lectorarchivos/LectorArchivos.java
+++ LectorArchivos/src/lectorarchivos/LectorArchivos.java
@@ -4,25 +4,43 @@
  */
 package lectorarchivos;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import javax.swing.JFileChooser;
 
 /**
  *
  * @author sl001
  */
 public class LectorArchivos {
-    public static void main(String[] args) {
-        String nombreArchivo = ".txt";
+    public static void main(String[] args) throws IOException {
+        JFileChooser archivo = new JFileChooser();
+        archivo.setCurrentDirectory(new File(System.getProperty("user.dir")));
+        int resultado = archivo.showOpenDialog(null);
 
-        try {
-            try (java.io.BufferedReader lector = new BufferedReader(new FileReader(nombreArchivo))) {
-                String linea;
-                
-                while ((linea = lector.readLine()) != null) {
-                    System.out.println(linea);
-                }
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            File seleccionado = archivo.getSelectedFile();
+            String nombreArchivo = seleccionado.getAbsolutePath();
+
+            leerArchivo(nombreArchivo);
+        } else {
+            System.out.println("No se seleccionó un archivo");
+        }
+    }
+
+    private static void leerArchivo(String nombreArchivo) throws IOException {
+        try (BufferedReader lector = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+
+            while ((linea = lector.readLine()) != null) {
+                System.out.println(linea);
             }
         } catch (IOException e) {
-            System.err.println("Error al leer el archivo: " + e.getMessage());
+            mostrarError(e);
         }
+    }
+
+    private static void mostrarError(IOException e) {
+        System.err.println("Error al leer el archivo: " + e.getMessage());
     }
 }
