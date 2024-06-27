35bf8b9c43d93ee6e78bcc6c02896f46a249ea4e
Laboratorio SL002 <sl002@unimet.local>
2024-06-27 15:11:54 -0400

    cambio 3.0

--- LectorArchivos/src/lectorarchivos/LectorArchivos.java
+++ LectorArchivos/src/lectorarchivos/LectorArchivos.java
@@ -14,27 +14,57 @@ import javax.swing.JFileChooser;
  */
 public class LectorArchivos {
     public static void main(String[] args) throws IOException {
-        JFileChooser archivo = new JFileChooser();
-        archivo.setCurrentDirectory(new File(System.getProperty("user.dir")));
-        int resultado = archivo.showOpenDialog(null);
+//        JFileChooser archivo = new JFileChooser();
+//        archivo.setCurrentDirectory(new File(System.getProperty("user.dir")));
+//        int resultado = archivo.showOpenDialog(null);
+//
+//        if (resultado == JFileChooser.APPROVE_OPTION) {
+//            File seleccionado = archivo.getSelectedFile();
+//            String nombreArchivo = seleccionado.getAbsolutePath();
+//            leerArchivo(nombreArchivo);
+//        } else {
+//            System.out.println("No se seleccionó un archivo");
+//        }
+          leerArchivo("../Archivos/FndCompl.txt");
+          leerArchivo("../Archivos/GraphQLREST.txt");
+          leerArchivo("../Archivos/gridconfig.txt");
+          leerArchivo("../Archivos/i18n.txt");
 
-        if (resultado == JFileChooser.APPROVE_OPTION) {
-            File seleccionado = archivo.getSelectedFile();
-            String nombreArchivo = seleccionado.getAbsolutePath();
-
-            leerArchivo(nombreArchivo);
-        } else {
-            System.out.println("No se seleccionó un archivo");
-        }
+          //Leer archivo deberia retornar el objeto que creen de Articulo Cientifico
+          // y luego lo insertan en la estructura de datos que utilicen
     }
 
-    private static void leerArchivo(String nombreArchivo) throws IOException {
+    public static void leerArchivo(String nombreArchivo) throws IOException {
         try (BufferedReader lector = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-
+            String titulo = "";
+            String autores = "";
+            String resumen = "";
+            String palabras_clave;
+            int modo = 1;
             while ((linea = lector.readLine()) != null) {
+                if(linea.equals("Autores")){
+                    modo = 2;
+                }else if(linea.equals("Resumen")){
+                    modo = 3;
+                }else if(linea.toLowerCase().contains("palabras clave")){
+                    linea = linea.toLowerCase().replace("palabras clave:", resumen);
+                    palabras_clave = linea;
+                    break;
+                }
+                
+                else if(modo == 1){
+                    if(!linea.isBlank()){
+                        titulo += linea;
+                    }
+                }else if (modo == 2){
+                    autores += linea + ",";
+                }else if (modo ==3){
+                    resumen += linea;
+                }
                 System.out.println(linea);
             }
+            //CREAR EL OBJETO DE ARTICULO
         } catch (IOException e) {
             mostrarError(e);
         }
